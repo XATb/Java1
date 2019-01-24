2dacc7bc19edeb4ea3704d41e931c5da28bfb3a0
XATb <alanis45@inbox.ru>
2019-01-24 13:57:37 +0300

    task5

---
+++ src/dr5/Klerk.java
@@ -0,0 +1,36 @@
+package dr5;
+
+public class Klerk {
+    String fio;
+    String rank;
+    String email;
+    String phone;
+    double cash;
+    int age;
+
+    Klerk (String _fio,String _rank,String _email,String _phone,double _cash,int _age){
+        fio = _fio;
+        rank = _rank;
+        email = _email;
+        phone = _phone;
+        cash = _cash;
+        age = _age;
+    }
+    static void bolshe40 (Klerk[]arrKlerks) {
+        for (Klerk x : arrKlerks) {
+            if (x.age > 40) {
+                System.out.println(x.toString());
+            }
+        }
+    }
+    public String toString() {
+        return "Klerk{" +
+                "fio='" + fio + '\'' +
+                ", rank='" + rank + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", cash=" + cash +
+                ", age=" + age +
+                '}';
+    }
+}
