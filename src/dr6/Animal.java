f83d9318dfeca24ac9be90f804759045936049d4
XATb <alanis45@inbox.ru>
2019-01-30 10:16:56 +0300

    task6

---
+++ src/dr6/Animal.java
@@ -0,0 +1,9 @@
+package dr6;
+
+public abstract class Animal {
+    public abstract boolean run(double run);
+
+    public abstract boolean jump(double jump);
+
+    public abstract boolean swim(double swim);
+}
